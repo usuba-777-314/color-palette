08a5359689b2f7985a183e3edbc54cc27bf72270
hironobu-igawa <zin920314@gmail.com>
2016-07-16 15:06:13 +0900

    Refactor

--- release/color-palette.js
+++ release/color-palette.js
@@ -146,11 +146,47 @@ var ColorPalette;
 (function (module) {
   'use strict';
 
-  Object.defineProperty(module, 'backgroundChangeFlag', {
-    get: function () {
-      return module.settings.backgroundChangeFlag;
+  module.BackgroundChanger = (function() {
+    function BackgroundChanger($target) {
+      this.$target = $target;
+      this.$target.on('change', this.change.bind(this));
     }
-  });
+
+    BackgroundChanger.init = function($target) {
+      return new BackgroundChanger($target);
+    };
+
+    BackgroundChanger.prototype.change = function() {
+      if (!this.isActive()) return;
+      this.$target
+        .css('background-color', this.color)
+        .css('color', this.isBright() ? '#000000' : '#FFFFFF');
+    };
+
+    BackgroundChanger.prototype.isActive = function() {
+      return module.settings.backgroundChangeFlag || false;
+    };
+
+    BackgroundChanger.prototype.isBright = function() {
+      var red = parseInt(this.color.substr(1, 2), 16);
+      var green = parseInt(this.color.substr(3, 2), 16);
+      var blue = parseInt(this.color.substr(5, 2), 16);
+      var max = Math.max(red, green, blue);
+      var min = Math.min(red, green, blue);
+      var saturation = ((max - min) / max) * 100;
+      var value = (max / 255) * 100;
+      return value - saturation >= 50;
+    };
+
+    Object.defineProperty(BackgroundChanger.prototype, 'color', {
+      get: function() {
+        var value = this.$target.val();
+        return module.RGB.isValid(value) ? value : '#FFFFFF';
+      }
+    });
+
+    return BackgroundChanger;
+  }());
 })(ColorPalette || (ColorPalette = {}));
 
 var ColorPalette;
@@ -159,15 +195,8 @@ var ColorPalette;
 
   module.ColorPalette = (function () {
     function ColorPalette(content) {
-      var _this = this;
       this.$target = $(content);
-
-      this.$target.on('change', function() {
-        if (!module.backgroundChangeFlag) return;
-        _this.$target
-          .css('background-color', _this.color)
-          .css('color', _this.isBright() ? '#000000' : '#FFFFFF');
-      });
+      this.backgroundChanger = module.BackgroundChanger.init(this.$target);
       this.closeListeners = [];
     }
 
@@ -298,24 +327,6 @@ var ColorPalette;
     ColorPalette.prototype.isOpen = function () {
       return !!this.$palette;
     };
-
-    ColorPalette.prototype.isBright = function() {
-      var red = parseInt(this.color.substr(1, 2), 16);
-      var green = parseInt(this.color.substr(3, 2), 16);
-      var blue = parseInt(this.color.substr(5, 2), 16);
-      var max = Math.max(red, green, blue);
-      var min = Math.min(red, green, blue);
-      var saturation = ((max - min) / max) * 100;
-      var value = (max / 255) * 100;
-      return value - saturation >= 50;
-    };
-
-    Object.defineProperty(ColorPalette.prototype, 'color', {
-      get: function() {
-        var value = this.$target.val();
-        return module.RGB.isValid(value) ? value : '#FFFFFF';
-      }
-    });
 
     Object.defineProperty(ColorPalette.prototype, 'xOffset', {
       get: function() {
--- src/scripts/background-change.js
+++ src/scripts/background-change.js
@@ -2,9 +2,45 @@ var ColorPalette;
 (function (module) {
   'use strict';
 
-  Object.defineProperty(module, 'backgroundChangeFlag', {
-    get: function () {
-      return module.settings.backgroundChangeFlag;
+  module.BackgroundChanger = (function() {
+    function BackgroundChanger($target) {
+      this.$target = $target;
+      this.$target.on('change', this.change.bind(this));
     }
-  });
+
+    BackgroundChanger.init = function($target) {
+      return new BackgroundChanger($target);
+    };
+
+    BackgroundChanger.prototype.change = function() {
+      if (!this.isActive()) return;
+      this.$target
+        .css('background-color', this.color)
+        .css('color', this.isBright() ? '#000000' : '#FFFFFF');
+    };
+
+    BackgroundChanger.prototype.isActive = function() {
+      return module.settings.backgroundChangeFlag || false;
+    };
+
+    BackgroundChanger.prototype.isBright = function() {
+      var red = parseInt(this.color.substr(1, 2), 16);
+      var green = parseInt(this.color.substr(3, 2), 16);
+      var blue = parseInt(this.color.substr(5, 2), 16);
+      var max = Math.max(red, green, blue);
+      var min = Math.min(red, green, blue);
+      var saturation = ((max - min) / max) * 100;
+      var value = (max / 255) * 100;
+      return value - saturation >= 50;
+    };
+
+    Object.defineProperty(BackgroundChanger.prototype, 'color', {
+      get: function() {
+        var value = this.$target.val();
+        return module.RGB.isValid(value) ? value : '#FFFFFF';
+      }
+    });
+
+    return BackgroundChanger;
+  }());
 })(ColorPalette || (ColorPalette = {}));
